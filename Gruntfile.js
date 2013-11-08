42952218f60345663949ace00e6bf392144329e5
JrSchild <joriami@gmail.com>
2013-11-08 12:21:50 +0100

    Importing working version of grunt-retinafy. Committing test-images, import npm packages, commenting out unit-tests for now.

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,64 +10,64 @@
 
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>',
-      ],
-      options: {
-        jshintrc: '.jshintrc',
-      },
-    },
+	// Project configuration.
+	grunt.initConfig({
+		jshint: {
+			all: [
+				'Gruntfile.js',
+				'tasks/*.js',
+				'<%= nodeunit.tests %>',
+			],
+			options: {
+				jshintrc: '.jshintrc',
+			},
+		},
 
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp'],
-    },
+		// Before generating any new files, remove any previously-created files.
+		clean: {
+			tests: ['tmp'],
+		},
 
-    // Configuration to be run (and then tested).
-    retinafy: {
-      default_options: {
-        options: {
-        },
-        files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
-      },
-      custom_options: {
-        options: {
-          separator: ': ',
-          punctuation: ' !!!',
-        },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
-      },
-    },
+		// Configuration to be run (and then tested).
+		retinafy: {
+			default_options: {
+				options: {
+					sizes: {
+						'50%': '1x/${name}@1x',
+						'75%': '1.5x/${name}@1.5x',
+						'100%': '2x/${name}@2x'
+					}
+				},
+				files: [{
+					expand: true,
+					cwd: 'test/fixtures',
+					src: ['**.{jpg,gif,png}'],
+					dest: 'tmp/default_options'
+				}],
+			}
+		},
 
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js'],
-    },
+		// Unit tests.
+		nodeunit: {
+			tests: ['test/*_test.js'],
+		},
 
-  });
+	});
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
+	// Actually load this plugin's task(s).
+	grunt.loadTasks('tasks');
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'retinafy', 'nodeunit']);
+	// Whenever the "test" task is run, first clean the "tmp" dir, then run this
+	// plugin's task(s), then test the result.
+	grunt.registerTask('test', ['clean', 'retinafy', 'nodeunit']);
 
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
+	// By default, lint and run all tests.
+	// grunt.registerTask('default', ['jshint', 'test']);
+	grunt.registerTask('default', ['test']);
 
 };
--- tasks/retinafy.js
+++ tasks/retinafy.js
@@ -10,41 +10,95 @@
 
 module.exports = function(grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+	var im = require('node-imagemagick');
+	var async = require('async');
+	var path = require('path');
 
-  grunt.registerMultiTask('retinafy', 'Take the 2x images and generate retina and regular versions', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
-    });
+	grunt.registerMultiTask('retinafy', 'Take the 2x images and generate retina and regular versions', function() {
+		var done = this.async();
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
+		// Merge task-specific and/or target-specific options with these defaults.
+		var options = this.options({
+			sizes: {}
+		});
 
-      // Handle options.
-      src += options.punctuation;
+		// cache regexes.
+		var r_percentage = /([0-9]+)%$/;
+		function processSize(targetSize, currSize) {
+			var match = (targetSize.match(r_percentage) || [])[1];
+			var sizes;
 
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
+			if (match /= 100) {
+				sizes = {
+					width: currSize.width * match,
+					height: 0
+				}
+				return sizes;
+			}
+		}
 
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });
-  });
+		function getPathTo(image, name) {
+			var parts = image.split('/');
+			var path = {};
+
+			path.file = parts.slice(-1)[0].replace('${name}', name);
+			if (parts.length > 1) {
+				path.path = '/' + parts.slice(0, -1).join('/');
+			}
+
+			return path;
+		}
+
+		this.files.forEach(function(f) {
+			var extName = path.extname(f.dest),
+				srcPath = f.src[0],
+				baseName = path.basename(srcPath, extName), // filename without extension
+				dirName,
+				dstPath;
+
+			// First get the dimensions of the file.
+			im.identify(f.src[0], function(err, features) {
+				if (err) {
+					throw err;
+				}
+
+				var currImageSize = {
+					width: features.width,
+					height: features.height
+				}
+
+				for (var size in options.sizes) {
+					var imagePath = getPathTo(options.sizes[size], baseName);
+
+					dirName = path.dirname(f.dest) + imagePath.path;
+
+					// Make directory if it doesn't exist.
+					if (!grunt.file.isDir(dirName)) {
+						grunt.file.mkdir(dirName);
+					}
+
+					dstPath = path.join(dirName, baseName + size + extName);
+
+					var dest = options.sizes[size];
+					var destImageSize = processSize(size, currImageSize);
+
+					im.resize({
+						srcPath: srcPath,
+						dstPath: dstPath,
+						format: extName.replace('.', ''),
+						width: destImageSize.width,
+						height: destImageSize.height
+					}, function(err, stdout, stderr) {
+						if (err) {
+							throw err;
+						}
+					});
+
+				}
+			}); /** /Identify */
+
+		}); /** /files.forEach */
+
+	}); /** /registerMultiTask */
 
 };
